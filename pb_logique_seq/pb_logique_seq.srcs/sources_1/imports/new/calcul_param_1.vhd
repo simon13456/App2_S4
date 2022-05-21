41d89da2f7744982b21ae6e660d7b56e8f47daf0
AB <bera1107@usherbrooke.ca>
2022-05-21 15:18:20 -0400

    barrel roll aka im an idiot sandwich

--- pb_logique_seq/pb_logique_seq.srcs/sources_1/imports/new/calcul_param_1.vhd
+++ pb_logique_seq/pb_logique_seq.srcs/sources_1/imports/new/calcul_param_1.vhd
@@ -8,7 +8,7 @@
 --    Version         : 5.0
 --    Nomenclature    : inspiree de la nomenclature 0.2 GRAMS
 --    Date            : 16 janvier 2020, 4 mai 2020
---    Auteur(s)       : bera1107 & lers0601
+--    Auteur(s)       : 
 --    Technologie     : ZYNQ 7000 Zybo Z7-10 (xc7z010clg400-1) 
 --    Outils          : vivado 2019.1 64 bits
 --
@@ -35,97 +35,28 @@ use UNISIM.vcomponents.all;
 ----------------------------------------------------------------------------------
 entity calcul_param_1 is
     Port (
-    i_bclk_P1    : in   std_logic; -- bit clock (I2S)
-    i_reset_P1   : in   std_logic;
-    i_en_P1      : in   std_logic; -- un echantillon present a l'entrée
-    i_ech_P1     : in   std_logic_vector (23 downto 0); -- echantillon en entrée
-    o_param_P1   : out  std_logic_vector (7 downto 0)   -- paramètre calculé
+    i_bclk    : in   std_logic; -- bit clock (I2S)
+    i_reset   : in   std_logic;
+    i_en      : in   std_logic; -- un echantillon present a l'entrée
+    i_ech     : in   std_logic_vector (23 downto 0); -- echantillon en entrée
+    o_param   : out  std_logic_vector (7 downto 0)   -- paramètre calculé
     );
 end calcul_param_1;
 
 ----------------------------------------------------------------------------------
 
 architecture Behavioral of calcul_param_1 is
---ajout d'un compteur
-component  compteur_nbits is
-generic (nbits : integer := 8);
-   port ( clk             : in    std_logic; 
-          i_en            : in    std_logic; 
-          reset           : in    std_logic; 
-          o_val_cpt       : out   std_logic_vector (nbits-1 downto 0)
-          );
-end component;
 
 ---------------------------------------------------------------------------------
 -- Signaux
 ----------------------------------------------------------------------------------
+    
 
-
-    -- state list
- type fsm_c_etats is (
-      sta_At,
-      sta_Va,
-      sta_Vb,
-      sta_Vc,
-      sta_Li
-     );
-signal fsm_EtatCourant, fsm_prochainEtat : fsm_c_etats; -- conserve le state
-signal cpt_previous, cpt_current, d_cpt, clk_By_Period : std_logic_vector (7 downto 0);
-
-signal d_reset, d_en, d_clk : std_logic;
 ---------------------------------------------------------------------------------------------
 --    Description comportementale
 ---------------------------------------------------------------------------------------------
 begin 
--- instance du compteur
-inst_cpt : compteur_nbits
- Port map(
-    clk             => d_clk,
-    i_en            => d_en,        
-    reset           => d_reset, 
-    o_val_cpt       => d_cpt 
- );
---Actualise le state
-    process(i_bclk_P1, i_reset_P1)
-    begin
-       if (i_reset_P1 ='1') then
-             fsm_EtatCourant <= sta_At;
-       else
-       if rising_edge(i_bclk_P1) then
-             fsm_EtatCourant <= fsm_prochainEtat;
-       end if;
-       end if;
-end process;
 
---MEF M5
-    process(i_bclk_P1)
-    begin
-      if i_ech_P1(23) = '0' then  
-        case fsm_EtatCourant is
-            when sta_Va =>
-                fsm_prochainEtat <= sta_Vb;
-            when sta_Vb =>
-                fsm_prochainEtat <= sta_Vb;
-            when sta_Vc =>
-                fsm_prochainEtat <= sta_Li;
-            when sta_Li => -- On envoie les données 
-                 fsm_prochainEtat <= sta_Li;
-                 cpt_current      <= d_cpt; 
-            when others => -- Agit comme sta_At
-                fsm_prochainEtat <= sta_At;
-                d_reset <= '1';
-                cpt_previous<= "00000000";
-        end case;
-      else
-        fsm_prochainEtat <= sta_At;
-        d_reset <= '1';
-        cpt_previous<= "00000000";         
-      end if;  
-    end process;
-    process(d_cpt)
-    begin
-    clk_By_Period <= cpt_current - cpt_previous;
-    d_reset <= '1';
-    cpt_previous<= "00000000";  
-    end process;
+     o_param <= x"01";    -- temporaire ...
+ 
 end Behavioral;
